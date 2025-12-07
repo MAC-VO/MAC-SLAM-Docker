187bdc7f8787e593656cab866f9bf79751cadcd5
Yutian Chen <yutianch@andrew.cmu.edu>
2025-12-07 02:59:28 -0500

    Final cleanup

--- Archive/L4T-Thor/run_airlab_thor.sh
+++
@@ -1,2 +0,0 @@
-./start_interact.sh /home/airlab/Documents/MAC/MAC-SLAM /home/airlab/Documents/MAC/SampleData/
-
--- Linux/Material/install_zedsdk.sh
+++ Linux/Material/install_zedsdk.sh
@@ -2,20 +2,18 @@
 set -e
 
 # 1. Detect the version
-# We rely on the $CUDA_VERSION environment variable provided by the base image.
+# We rely on the $CUDA_MAJOR_VERSION environment variable provided by the base image.
 # If it's not set, default to a fallback or exit error.
 ARCH=$(uname -m)
 DETAIL_ARCH=$(uname -r)
 
 
 echo "Running installation script..."
-MAJOR_VER=$(nvidia-smi | grep -oP 'CUDA Version:\s*\K[0-9]+' | head -n1 || true)
-echo "Detected full CUDA Version: $MAJOR_VER"
 
 # 2. Conditional Logic based on architecture.
 if [[ "$ARCH" = "x86_64" ]]; then
 
-    if [[ "$MAJOR_VER" = "12" ]]; then
+    if [[ "$CUDA_MAJOR_VERSION" = "12" ]]; then
         echo ">>> Branch: CUDA 12 Detected."
         echo ">>> Installing ZedSDK for CUDA 12..."
         
@@ -24,18 +22,18 @@ if [[ "$ARCH" = "x86_64" ]]; then
             chmod +x /tmp/zed_sdk.run && \
             /tmp/zed_sdk.run -- silent runtime_only skip_tools skip_od_module
 
-    elif [[ "$MAJOR_VER" = "13" ]]; then
+    elif [[ "$CUDA_MAJOR_VERSION" = "13" ]]; then
         echo ">>> Branch: CUDA 13 Detected."
         echo ">>> Installing ZedSDK for CUDA 13..."
 
         # commands for CUDA 13
-        wget -O /tmp/zed_sdk.run https://download.stereolabs.com/zedsdk/5.1/cu13/ubuntu24 && \
+        wget -O /tmp/zed_sdk.run https://download.stereolabs.com/zedsdk/4.2/cu12/ubuntu24 && \
             chmod +x /tmp/zed_sdk.run && \
             /tmp/zed_sdk.run -- silent runtime_only skip_tools skip_od_module
 
     else
-        echo "!!! Error: Unsupported CUDA Major Version: $MAJOR_VER"
-        exit 1
+        echo "!!! Error: Unsupported CUDA Major Version: $CUDA_MAJOR_VERSION"
+        exit 1  
     fi
 
 elif [[ "$DETAIL_ARCH" =~ "tegra" && "$ARCH" = "aarch64" ]]; then
@@ -45,7 +43,7 @@ elif [[ "$DETAIL_ARCH" =~ "tegra" && "$ARCH" = "aarch64" ]]; then
         chmod +x /tmp/zed_sdk.run && \
         /tmp/zed_sdk.run -- silent runtime_only skip_tools skip_od_module
 
-elif [ "$MAJOR_VER" = "13" ]; then
+elif [ "$CUDA_MAJOR_VERSION" = "13" ]; then
     echo ">>> Branch: CUDA 13 Detected."
     echo ">>> Installing ZedSDK for CUDA 13..."
 
@@ -55,7 +53,7 @@ elif [ "$MAJOR_VER" = "13" ]; then
         /tmp/zed_sdk.run -- silent runtime_only skip_tools skip_od_module
 
 else
-    echo "!!! Error: Unsupported CUDA Major Version: $MAJOR_VER"
+    echo "!!! Error: Unsupported CUDA Major Version: $CUDA_MAJOR_VERSION"
     exit 1
 fi
 
