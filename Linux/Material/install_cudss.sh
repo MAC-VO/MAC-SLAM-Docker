e29f91066ec433aff7654bca270e6f24aaba4846
Yutian Chen <yutianch@andrew.cmu.edu>
2025-12-06 22:58:09 -0500

    Improved installer

--- Linux/Material/install_cudss.sh
+++ Linux/Material/install_cudss.sh
@@ -1,6 +1,40 @@
-wget https://developer.download.nvidia.com/compute/cudss/0.6.0/local_installers/cudss-local-repo-ubuntu2404-0.6.0_0.6.0-1_amd64.deb && \
-    dpkg -i cudss-local-repo-ubuntu2404-0.6.0_0.6.0-1_amd64.deb && \
-    cp /var/cudss-local-repo-ubuntu2404-0.6.0/cudss-*-keyring.gpg /usr/share/keyrings/ && \
-    apt-get update && \
-    apt-get -y install cudss && \
-    rm ./cudss-local-repo-ubuntu2404-0.6.0_0.6.0-1_amd64.deb
+#!/bin/bash
+set -e
+
+ARCH=$(uname -m)
+DETAIL_ARCH=$(uname -r)
+
+if [[ "$ARCH" = "x86_64" ]]; then
+    
+    # Install CUDSS for x86-64 architecture
+    wget https://developer.download.nvidia.com/compute/cudss/0.6.0/local_installers/cudss-local-repo-ubuntu2404-0.6.0_0.6.0-1_amd64.deb && \
+        dpkg -i cudss-local-repo-ubuntu2404-0.6.0_0.6.0-1_amd64.deb && \
+        cp /var/cudss-local-repo-ubuntu2404-0.6.0/cudss-*-keyring.gpg /usr/share/keyrings/ && \
+        apt-get update && \
+        apt-get -y install cudss && \
+        rm ./cudss-local-repo-ubuntu2404-0.6.0_0.6.0-1_amd64.deb
+
+elif [[ "$DETAIL_ARCH" =~ "tegra" && "$ARCH" = "aarch64" ]]; then
+
+    # Install CUDSS for Jetpack (NVIDIA Orin) architecture
+    wget https://developer.download.nvidia.com/compute/cudss/0.6.0/local_installers/cudss-local-tegra-repo-ubuntu2404-0.6.0_0.6.0-1_arm64.deb && \
+        dpkg -i cudss-local-tegra-repo-ubuntu2404-0.6.0_0.6.0-1_arm64.deb && \
+        cp /var/cudss-local-tegra-repo-ubuntu2404-0.6.0/cudss-*-keyring.gpg /usr/share/keyrings/ && \
+        apt-get update && \
+        apt-get -y install cudss && \
+        rm ./cudss-local-tegra-repo-ubuntu2404-0.6.0_0.6.0-1_arm64.deb
+
+elif [[ "$ARCH" = "aarch64" ]]; then
+
+    # Install CUDSS for ARM SBSA (NVIDIA Thor) architecture
+    wget https://developer.download.nvidia.com/compute/cudss/0.6.0/local_installers/cudss-local-repo-ubuntu2404-0.6.0_0.6.0-1_arm64.deb && \
+        dpkg -i cudss-local-repo-ubuntu2404-0.6.0_0.6.0-1_arm64.deb && \
+        cp /var/cudss-local-repo-ubuntu2404-0.6.0/cudss-*-keyring.gpg /usr/share/keyrings/ && \
+        apt-get update && \
+        apt-get -y install cudss && \
+        rm ./cudss-local-repo-ubuntu2404-0.6.0_0.6.0-1_arm64.deb
+    
+else
+    echo "Error: Unsupported architecture: $ARCH"
+    exit 1
+fi
--- Linux/Material/install_zedsdk.sh
+++ Linux/Material/install_zedsdk.sh
@@ -4,21 +4,44 @@ set -e
 # 1. Detect the version
 # We rely on the $CUDA_VERSION environment variable provided by the base image.
 # If it's not set, default to a fallback or exit error.
-CURRENT_VER=${CUDA_VERSION:-"unknown"}
+ARCH=$(uname -m)
+DETAIL_ARCH=$(uname -r)
+
 
 echo "Running installation script..."
-echo "Detected full CUDA Version: $CURRENT_VER"
+MAJOR_VER=$(nvidia-smi | grep -oP 'CUDA Version:\s*\K[0-9]+' | head -n1 || true)
+echo "Detected full CUDA Version: $MAJOR_VER"
 
-# Extract the major version (e.g., "12" from "12.2.0")
-MAJOR_VER=$(echo "$CURRENT_VER" | cut -d. -f1)
+# 2. Conditional Logic based on architecture.
+if [[ "$ARCH" = "x86_64" ]]; then
 
-# 2. Conditional Logic
-if [ "$MAJOR_VER" = "12" ]; then
-    echo ">>> Branch: CUDA 12 Detected."
-    echo ">>> Installing ZedSDK for CUDA 12..."
-    
-    # commands for CUDA 12
-    wget -O /tmp/zed_sdk.run https://download.stereolabs.com/zedsdk/5.1/cu12/ubuntu24 && \
+    if [[ "$MAJOR_VER" = "12" ]]; then
+        echo ">>> Branch: CUDA 12 Detected."
+        echo ">>> Installing ZedSDK for CUDA 12..."
+        
+        # commands for CUDA 12
+        wget -O /tmp/zed_sdk.run https://download.stereolabs.com/zedsdk/4.2/cu12/ubuntu24 && \
+            chmod +x /tmp/zed_sdk.run && \
+            /tmp/zed_sdk.run -- silent runtime_only skip_tools skip_od_module
+
+    elif [[ "$MAJOR_VER" = "13" ]]; then
+        echo ">>> Branch: CUDA 13 Detected."
+        echo ">>> Installing ZedSDK for CUDA 13..."
+
+        # commands for CUDA 13
+        wget -O /tmp/zed_sdk.run https://download.stereolabs.com/zedsdk/5.1/cu13/ubuntu24 && \
+            chmod +x /tmp/zed_sdk.run && \
+            /tmp/zed_sdk.run -- silent runtime_only skip_tools skip_od_module
+
+    else
+        echo "!!! Error: Unsupported CUDA Major Version: $MAJOR_VER"
+        exit 1
+    fi
+
+elif [[ "$DETAIL_ARCH" =~ "tegra" && "$ARCH" = "aarch64" ]]; then
+
+    # command for CUDA 12.6, Orin AGX
+    wget -O /tmp/zed_sdk.run https://download.stereolabs.com/zedsdk/4.2/l4t36.4/jetsons && \
         chmod +x /tmp/zed_sdk.run && \
         /tmp/zed_sdk.run -- silent runtime_only skip_tools skip_od_module
 
